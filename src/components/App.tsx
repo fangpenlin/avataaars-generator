748dbf95db902911539aa0296ac1c011ed9bdafe
Fang-Pen Lin <hello@fangpenlin.com>
2017-11-17 00:38:31 -0800

    Implement saving image

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import '../assets/App.css'
 
 import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
 import AvatarForm from './AvatarForm'
 import Avatar, { EyeType, EyebrowType } from './avatar'
@@ -19,6 +20,9 @@ export default class App extends React.Component<{}, State> {
     clotheType: ClotheType.BlazerShirt
   }
 
+  private avatarRef: Avatar | null = null
+  private canvasRef: HTMLCanvasElement | null = null
+
   render () {
     const { eyeType, eyebrowType, clotheType } = this.state
     return (
@@ -28,6 +32,7 @@ export default class App extends React.Component<{}, State> {
             eyeType={eyeType}
             eyebrowType={eyebrowType}
             clotheType={clotheType}
+            ref={this.onAvatarRef}
           />
         </div>
         <AvatarForm
@@ -39,8 +44,22 @@ export default class App extends React.Component<{}, State> {
           onClotheChange={this.onClotheChange}
           onDownload={this.onDownload}
         />
+        <canvas
+          style={{ display: 'none' }}
+          width='240'
+          height='262'
+          ref={this.onCanvasRef}
+        />
       </div>
     )
+  }
+
+  private onAvatarRef = (ref: Avatar) => {
+    this.avatarRef = ref
+  }
+
+  private onCanvasRef = (ref: HTMLCanvasElement) => {
+    this.canvasRef = ref
   }
 
   private onEyeChange = (eyeType: EyeType) => {
@@ -56,6 +75,39 @@ export default class App extends React.Component<{}, State> {
   }
 
   private onDownload = () => {
-    console.info('@@@@@@')
+    const svgNode = ReactDOM.findDOMNode(this.avatarRef!)
+    const ctx = this.canvasRef!.getContext('2d')!
+
+    const anyWindow = window as any
+    const DOMURL = anyWindow.URL || anyWindow.webkitURL || window
+
+    const data = svgNode.outerHTML
+    const img = new Image()
+    const svg = new Blob([data], { type: 'image/svg+xml' })
+    const url = DOMURL.createObjectURL(svg)
+
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0)
+      DOMURL.revokeObjectURL(url)
+      const imageURI = this.canvasRef!
+        .toDataURL('image/png')
+        .replace('image/png', 'image/octet-stream')
+      this.triggerDownload(imageURI)
+    }
+    img.src = url
+  }
+
+  private triggerDownload (imgURI: string) {
+    const event = new MouseEvent('click', {
+      view: window,
+      bubbles: false,
+      cancelable: true
+    })
+
+    const a = document.createElement('a')
+    a.setAttribute('download', 'avataaars.png')
+    a.setAttribute('href', imgURI)
+    a.setAttribute('target', '_blank')
+    a.dispatchEvent(event)
   }
 }
