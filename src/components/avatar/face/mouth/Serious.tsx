16c1d10ac1de918ca8c43d32f28b9fa36fa76c2d
Fang-Pen Lin <hello@fangpenlin.com>
2017-11-18 00:55:29 -0800

    Add mouths

---
+++ src/components/avatar/face/mouth/Serious.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+export default class Serious extends React.Component {
+  render () {
+    return (
+      <g
+        id='Mouth/Serious'
+        transform='translate(2.000000, 52.000000)'
+        fill='#000000'
+        fill-opacity='0.699999988'>
+        <rect id='Why-so-serious?' x='42' y='18' width='24' height='6' rx='3' />
+      </g>
+    )
+  }
+}
