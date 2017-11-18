db905e5930d2c6139852c27e6672d0417feadcb4
Fang-Pen Lin <hello@fangpenlin.com>
2017-11-18 01:18:43 -0800

    More cloth

--- src/components/avatar/clothes/index.tsx
+++ src/components/avatar/clothes/index.tsx
@@ -2,13 +2,25 @@ import * as React from 'react'
 
 import BlazerShirt from './BlazerShirt'
 import BlazerSweater from './BlazerSweater'
+import CollarSweater from './CollarSweater'
+import GraphicShirt from './GraphicShirt'
+import Hoodie from './Hoodie'
 
 export enum Type {
   BlazerShirt = 'BlazerShirt',
-  BlazerSweater = 'BlazerSweater'
+  BlazerSweater = 'BlazerSweater',
+  CollarSweater = 'CollarSweater',
+  GraphicShirt = 'GraphicShirt',
+  Hoodie = 'Hoodie'
 }
 
-export const AllTypes = [Type.BlazerShirt, Type.BlazerSweater]
+export const AllTypes = [
+  Type.BlazerShirt,
+  Type.BlazerSweater,
+  Type.CollarSweater,
+  Type.GraphicShirt,
+  Type.Hoodie
+]
 
 export interface Props {
   type: Type
@@ -21,6 +33,12 @@ export default class Clothes extends React.Component<Props> {
         return <BlazerShirt />
       case Type.BlazerSweater:
         return <BlazerSweater />
+      case Type.CollarSweater:
+        return <CollarSweater />
+      case Type.GraphicShirt:
+        return <GraphicShirt />
+      case Type.Hoodie:
+        return <Hoodie />
     }
     return null
   }
